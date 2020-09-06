63cd0a35b6d2676de25b90eb3ff9061de702d7fd
EDusik <eduardodusik@gmail.com>
2020-09-06 18:51:02 -0300

    Clear rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -5,8 +5,23 @@ import { createStage } from '../gameHelpers';
 export const useStage = (player, resetPlayer) => {
     
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] =  useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage => {
+            newStage.reduce((accumulator, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(previousStage => previousStage + 1);
+                    accumulator.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return accumulator;
+                }
+                accumulator.push(row);
+                return accumulator;
+            }, []);
+        }
+
         const updateStage = previousStage => {
             const newStage = previousStage.map(row => 
                 row.map(cell => (
@@ -29,15 +44,14 @@ export const useStage = (player, resetPlayer) => {
             // Then check if we collided
             if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
             
             return newStage;
         };
     
         setStage(previousStage => updateStage(previousStage));
-    }, [    
-        player, resetPlayer
-    ]);
+    }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 }
